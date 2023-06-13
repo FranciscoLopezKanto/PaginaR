9fea68294a4d6f74d27a429d628cb5574805fa96
FranciscoLopezKanto <franciscolopmez599@gmail.com>
2023-06-13 01:50:09 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect } from 'react';
-import './app.css';
+import './globals.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
